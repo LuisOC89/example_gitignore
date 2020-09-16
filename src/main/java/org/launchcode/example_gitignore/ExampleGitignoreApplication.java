da51ed13a83d4d2a10f6825d9dde0c539787f9e5
Luis Humberto Orozco Cabrera <humbledore@gmail.com>
2020-09-15 20:09:38 -0400

    Initial commit

---
+++ src/main/java/org/launchcode/example_gitignore/ExampleGitignoreApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.example_gitignore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleGitignoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleGitignoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/example_gitignore/ExampleGitignoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.example_gitignore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleGitignoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
